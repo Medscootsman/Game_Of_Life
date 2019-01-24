ecf9ae0b0458341a56c5e6a1252dd289926fd9be
Murray Lyne <murraylyne2@gmail.com>
2019-01-24 18:44:49 +0000

    added 2 classes, skeleton.

---
+++ Game_Of_Life/Cell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//this is the cell, each cell object can keep track of it's neighbours.
+namespace Game_Of_Life
+{
+    class Cell
+    {
+        bool alive;
+        int maxNeighbours = 3;
+        int minNeighbours = 2;
+        int currentNeighbours;
+
+        Cell N_Neighbour;
+        Cell NE_Neighbour;
+        Cell E_Neighbour;
+        Cell SE_Neighbour;
+        Cell S_Neighbour;
+        Cell SW_Neighbour;
+        Cell W_Neighbour;
+        Cell NW_Neighbour;
+
+
+
+    }
+}
---
+++ Game_Of_Life/Grid.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//this creates the grid and handles game logic.
+namespace Game_Of_Life
+{
+    class Grid
+    {
+        Cell[,] gameGrid;
+
+        public Grid()
+        {
+            this.gameGrid = new Cell[Int64.MaxValue, Int64.MaxValue];
+        }
+    }
+}
